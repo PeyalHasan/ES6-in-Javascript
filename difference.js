f842c3eae64036d62b4b97ce5a7efd5b8b8a308c
unknown <hpeyal7@gmail.com>
2024-12-23 17:30:22 +0600

    spread Operator.js

--- difference.js
+++ difference.js
@@ -5,26 +5,7 @@ function show(){
 show(); //window object
 
 const show1 = () =>{
-    console.log(this);
+    // console.log(this); // it will show an empty object because arrow function does not have its own this keyword. 
 }
 show1();
-
-//// Use of spread operator 
-
-const max = Math.max(23, 54, 33, 543, 64, 64);
-console.log(max);
-
-const numbers = [23, 54, 33, 53, 51, 64, 34]
-console.log(numbers);
-const max1 = Math.max(numbers);
-console.log(max1);//NaN because it is an array not a number  so we have use spread operator to solve this problem. 
-const max2 = Math.max(...numbers);
-console.log(max2);
-
-//// Use of reset operator  rest operator is used to pass a variable number of arguments to a function. 
-// create a reset operator in arrow function 
-const sum = (...args) => {
-    console.log(args);
-}
-sum(1,2,3,4,5,6,7,8,9,10)
 
